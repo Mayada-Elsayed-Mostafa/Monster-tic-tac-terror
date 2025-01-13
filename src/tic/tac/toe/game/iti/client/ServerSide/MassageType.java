eb4e9e8ef70d70f884c36cb15063e248ef14537b
Hazem-web <hazem.mah2722@gmail.com>
2025-01-13 19:39:50 +0200

    add server handler and massage type (#6)

---
+++ src/tic/tac/toe/game/iti/client/ServerSide/MassageType.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tic.tac.toe.game.iti.client.ServerSide;
+
+/**
+ *
+ * @author HAZEM-LAB
+ */
+public class MassageType {
+    public static final String LOGIN_MSG="login";
+    public static final String REGISTER_MSG="register";
+    public static final String UPDATE_LIST_MSG="update";
+    public static final String SERVER_CLOSE_MSG="server close";
+    public static final String CHALLENGE_REQUEST_MSG="challenge req";
+    public static final String CHALLENGE_ACCESSEPT_MSG="challenge acc";
+    public static final String PLAY_MSG="play";
+    public static final String END_GAME_MSG="end game";
+    public static final String RESTART_GAME_MSG="restart game";
+    public static final String CLIENT_CLOSE_MSG="client close";
+    
+}
