b480138d1cbd20f584722b188d1054979d15c4a1
Mayada Elsayed <140763552+Mayada-Elsayed-Mostafa@users.noreply.github.com>
2025-01-15 19:16:04 +0200

    Add request ip (#13)
    * add a function for request ip address but not handle yet
    
    * handle RuntimeException
    
    * edit the place of request ip function

--- src/tic/tac/toe/game/iti/client/WelcomeController.java
+++ src/tic/tac/toe/game/iti/client/WelcomeController.java
@@ -1,6 +1,7 @@
 package tic.tac.toe.game.iti.client;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
@@ -8,14 +9,20 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
 import tic.tac.toe.game.iti.client.Registeration.LoginPageController;
+import tic.tac.toe.game.iti.client.ServerSide.ServerHandler;
 import tic.tac.toe.game.iti.client.Singlemode.SinglemodeController;
 
 public class WelcomeController {
 
     private Stage stage;
+    private String ip;
+    private boolean isIpEntered = false;
 
     @FXML
     private Button multiModeBtn;
@@ -26,22 +33,25 @@ public class WelcomeController {
 
     public void setStage(Stage stage) {
         this.stage = stage;
-
     }
 
     @FXML
     public void goToOnlineGame() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("Registeration/LoginPage.fxml"));
-            Parent root = loader.load();
+        if (!isIpEntered) {
+            showAlertForIPAdress();
+        } if(isIpEntered) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Registeration/LoginPage.fxml"));
+                Parent root = loader.load();
 
-            LoginPageController controller = loader.getController();
-            controller.setStage(stage);
+                LoginPageController controller = loader.getController();
+                controller.setStage(stage);
 
-            stage.setScene(new Scene(root));
-            stage.setTitle("Login Page");
-        } catch (IOException ex) {
-            Logger.getLogger(WelcomeController.class.getName()).log(Level.SEVERE, null, ex);
+                stage.setScene(new Scene(root));
+                stage.setTitle("Login Page");
+            } catch (IOException ex) {
+                Logger.getLogger(WelcomeController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 
@@ -78,6 +88,28 @@ public class WelcomeController {
         } catch (IOException ex) {
             Logger.getLogger(SinglemodeController.class.getName()).log(Level.SEVERE, "Error loading Singlemode.fxml", ex);
         }
+    }
 
+    public void showAlertForIPAdress() {
+        TextInputDialog ipTI = new TextInputDialog();
+        ipTI.setTitle("IP Address");
+        ipTI.setHeaderText("Enter the IP Address...");
+
+        Optional<String> ipInput = ipTI.showAndWait();
+
+        if (ipInput.isPresent() && !ipInput.get().trim().isEmpty()) {
+            ip = ipInput.get().trim();
+            try {
+                ServerHandler.setSocket(ip);
+                isIpEntered = true;
+            } catch (IOException ex) {
+                Alert alert = new Alert(Alert.AlertType.WARNING, "Unable to connect to the server. Please try again later.", ButtonType.OK);
+                alert.setTitle("Connection Error");
+                alert.showAndWait();
+            }
+        } else {
+            Alert a = new Alert(Alert.AlertType.WARNING, "Please Enter IP to continue...", ButtonType.OK);
+            a.showAndWait();
+        }
     }
 }
