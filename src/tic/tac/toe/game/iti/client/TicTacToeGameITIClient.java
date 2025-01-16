e5c679435ea18a75dd17c3a66c5755ce374cf948
Hazem-web <hazem.mah2722@gmail.com>
2025-01-17 00:35:54 +0200

    Client close (#19)
    * add sign up
    
    * add server handler and massage type
    
    * add home ui
    
    * add image
    
    * Sign up
    
    * sign up
    
    * delete duplcuated folder
    
    * delete duplcuated folder
    
    * start client server handler
    
    * handle intial client close event
    
    ---------
    
    Co-authored-by: Aya <ayaemam471@gmail.com>

--- src/tic/tac/toe/game/iti/client/TicTacToeGameITIClient.java
+++ src/tic/tac/toe/game/iti/client/TicTacToeGameITIClient.java
@@ -1,10 +1,16 @@
 package tic.tac.toe.game.iti.client;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.json.simple.JSONObject;
+import tic.tac.toe.game.iti.client.ServerSide.MassageType;
+import tic.tac.toe.game.iti.client.ServerSide.ServerHandler;
 
 public class TicTacToeGameITIClient extends Application {
 
@@ -16,7 +22,15 @@ public class TicTacToeGameITIClient extends Application {
 
             WelcomeController controller = loader.getController();
             controller.setStage(primaryStage);
-
+            primaryStage.setOnCloseRequest((event) -> {
+                if(ServerHandler.socket!=null){
+                    try {
+                        ServerHandler.closeSocket();
+                    } catch (IOException ex) {
+                        Logger.getLogger(TicTacToeGameITIClient.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            });
             primaryStage.setScene(new Scene(root));
             primaryStage.setTitle("Welcome Page");
             primaryStage.show();
