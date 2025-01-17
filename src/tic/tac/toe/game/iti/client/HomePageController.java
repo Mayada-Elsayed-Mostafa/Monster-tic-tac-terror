8597ef36cb0c77cb44da25d28760ae77bb94ebad
Khairy Suleiman Jr. <128572079+KhairySuleiman4@users.noreply.github.com>
2025-01-17 23:57:52 +0300

    Update available list (#20)
    * Signup (#10)
    
    * add dtoPlayer class (#1)
    
    * add a logic and ui of welcome and game pages (#2)
    
    * add a logic and ui of welcome and game pages (#3)
    
    * add sign up
    
    * add sign up (#4)
    
    * Single mode (#5)
    
    * add sign up
    
    * add single mode & easy
    
    * add server handler and massage type (#6)
    
    * Home page (#7)
    
    * add server handler and massage type
    
    * add home ui
    
    * Home page (#8)
    
    * add server handler and massage type
    
    * add home ui
    
    * add image
    
    * Login (#9)
    
    * add dtoPlayer class
    
    * add login
    
    * solve a logical error
    
    * Sign up
    
    ---------
    
    Co-authored-by: shereen mohamed <89316475+shereenmohamed923@users.noreply.github.com>
    Co-authored-by: Mayada Elsayed <140763552+Mayada-Elsayed-Mostafa@users.noreply.github.com>
    Co-authored-by: Hazem-web <hazem.mah2722@gmail.com>
    Co-authored-by: Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
    
    * Revert "Signup (#10)" (#11)
    
    This reverts commit 4786db8b5e4ca1e1e8210887679bbf52256115d1.
    
    * List of available players updated
    
    ---------
    
    Co-authored-by: aya-emam-0 <ayaemam471@gmail.com>
    Co-authored-by: shereen mohamed <89316475+shereenmohamed923@users.noreply.github.com>
    Co-authored-by: Mayada Elsayed <140763552+Mayada-Elsayed-Mostafa@users.noreply.github.com>
    Co-authored-by: Hazem-web <hazem.mah2722@gmail.com>
    Co-authored-by: Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>

--- src/tic/tac/toe/game/iti/client/HomePageController.java
+++ src/tic/tac/toe/game/iti/client/HomePageController.java
@@ -5,18 +5,20 @@
  */
 package tic.tac.toe.game.iti.client;
 
+import java.util.List;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import tic.tac.toe.game.iti.client.player.Player;
 
 /**
  *
  * @author HAZEM-LAB
  */
 public class HomePageController {
-    
-    
     
     private Stage stage;
     
@@ -27,12 +29,42 @@ public class HomePageController {
     @FXML
     private VBox challenges;
     
+    private static VBox sUserNames;
+    private static VBox sScores;
+    private static VBox sChallenges;
+    
     public void setStage(Stage stage) {
         this.stage = stage;
-
+        sUserNames = usernames;
+        sScores = scores;
+        sChallenges = challenges;
     }
     
     public void handleLogout(ActionEvent event){
+        
+    }
+    
+    public static void updateAvailablePlayers(List<Player> players){
+        sUserNames.getChildren().clear();
+        sScores.getChildren().clear();
+        sChallenges.getChildren().clear();
+        
+        for(Player player : players){
+            Label user = new Label(player.getUserName());
+            sUserNames.getChildren().add(user);
+            
+            Label score = new Label(String.valueOf(player.getScore()));
+            sScores.getChildren().add(score);
+            
+            Button challengeBtn = new Button("Challenge");
+            challengeBtn.setOnAction(event -> {
+                handleChallenge(player);
+            });
+            sChallenges.getChildren().add(challengeBtn);
+        }
+    }
+    
+    private static void handleChallenge(Player player) {
         
     }
 }
