c04d66b730508e5a571cdf67f157db12c195c4de
Hazem-web <hazem.mah2722@gmail.com>
2025-01-22 20:21:45 +0200

    Save records (#52)
    * add sign up
    
    * add server handler and massage type
    
    * add home ui
    
    * add image
    
    * Sign up
    
    * Signup (#10)
    
    * add dtoPlayer class (#1)
    
    * add a logic and ui of welcome and game pages (#2)
    
    * add a logic and ui of welcome and game pages (#3)
    
    * add sign up
    
    * add sign up (#4)
    
    * Single mode (#5)
    
    * add sign up
    
    * add single mode & easy
    
    * add server handler and massage type (#6)
    
    * Home page (#7)
    
    * add server handler and massage type
    
    * add home ui
    
    * Home page (#8)
    
    * add server handler and massage type
    
    * add home ui
    
    * add image
    
    * Login (#9)
    
    * add dtoPlayer class
    
    * add login
    
    * solve a logical error
    
    * Sign up
    
    ---------
    
    Co-authored-by: shereen mohamed <89316475+shereenmohamed923@users.noreply.github.com>
    Co-authored-by: Mayada Elsayed <140763552+Mayada-Elsayed-Mostafa@users.noreply.github.com>
    Co-authored-by: Hazem-web <hazem.mah2722@gmail.com>
    Co-authored-by: Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
    
    * Revert "Signup (#10)" (#11)
    
    This reverts commit 4786db8b5e4ca1e1e8210887679bbf52256115d1.
    
    * sign up
    
    * delete duplcuated folder
    
    * delete duplcuated folder
    
    * start client server handler
    
    * handle intial client close event
    
    * List of available players updated
    
    * add client logout
    
    * add client logout
    
    * bugs solved
    
    * fix error
    
    * handle in between game
    
    * offline record page
    
    * remove json file
    
    * save records
    
    ---------
    
    Co-authored-by: Aya <ayaemam471@gmail.com>
    Co-authored-by: shereen mohamed <89316475+shereenmohamed923@users.noreply.github.com>
    Co-authored-by: Mayada Elsayed <140763552+Mayada-Elsayed-Mostafa@users.noreply.github.com>
    Co-authored-by: Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
    Co-authored-by: KhairySuleiman4 <khairy.hatem4@gmail.com>

--- src/tic/tac/toe/game/iti/client/Singlemode/EasymodeController.java
+++ src/tic/tac/toe/game/iti/client/Singlemode/EasymodeController.java
@@ -24,8 +24,6 @@ import tic.tac.toe.game.iti.client.WelcomeController;
 public class EasymodeController extends Controller implements Initializable {
 
     Stage stage;
-    FileWriter fw = null;
-
     @FXML
     private Text gameStatus;
     @FXML
@@ -292,10 +290,7 @@ public class EasymodeController extends Controller implements Initializable {
     @FXML
     private void endHandeler() {
         try {
-            if (fw != null) {
-                fw.close();
-            }
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/tic/tac/toe/game/iti/client/Singlemode/Singlemode.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/tic/tac/toe/game/iti/client/Welcome.fxml"));
             Parent root = loader.load();
 
             SinglemodeController controller = loader.getController();
