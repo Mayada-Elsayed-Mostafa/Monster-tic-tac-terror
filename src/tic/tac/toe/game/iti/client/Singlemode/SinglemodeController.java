2acee4179366eb89fcb21d1b29885f2db55388c0
Khairy Suleiman Jr. <128572079+KhairySuleiman4@users.noreply.github.com>
2025-01-23 11:36:33 +0300

    Adding back buttons (#56)
    * Signup (#10)
    
    * add dtoPlayer class (#1)
    
    * add a logic and ui of welcome and game pages (#2)
    
    * add a logic and ui of welcome and game pages (#3)
    
    * add sign up
    
    * add sign up (#4)
    
    * Single mode (#5)
    
    * add sign up
    
    * add single mode & easy
    
    * add server handler and massage type (#6)
    
    * Home page (#7)
    
    * add server handler and massage type
    
    * add home ui
    
    * Home page (#8)
    
    * add server handler and massage type
    
    * add home ui
    
    * add image
    
    * Login (#9)
    
    * add dtoPlayer class
    
    * add login
    
    * solve a logical error
    
    * Sign up
    
    ---------
    
    Co-authored-by: shereen mohamed <89316475+shereenmohamed923@users.noreply.github.com>
    Co-authored-by: Mayada Elsayed <140763552+Mayada-Elsayed-Mostafa@users.noreply.github.com>
    Co-authored-by: Hazem-web <hazem.mah2722@gmail.com>
    Co-authored-by: Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
    
    * Revert "Signup (#10)" (#11)
    
    This reverts commit 4786db8b5e4ca1e1e8210887679bbf52256115d1.
    
    * List of available players updated
    
    * bugs solved
    
    * back buttons added
    
    ---------
    
    Co-authored-by: aya-emam-0 <ayaemam471@gmail.com>
    Co-authored-by: shereen mohamed <89316475+shereenmohamed923@users.noreply.github.com>
    Co-authored-by: Mayada Elsayed <140763552+Mayada-Elsayed-Mostafa@users.noreply.github.com>
    Co-authored-by: Hazem-web <hazem.mah2722@gmail.com>
    Co-authored-by: Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>

--- src/tic/tac/toe/game/iti/client/Singlemode/SinglemodeController.java
+++ src/tic/tac/toe/game/iti/client/Singlemode/SinglemodeController.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tic.tac.toe.game.iti.client.Singlemode;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,6 +13,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import tic.tac.toe.game.iti.client.WelcomeController;
 
 public class SinglemodeController implements Initializable {
 
@@ -27,6 +25,8 @@ public class SinglemodeController implements Initializable {
     private Button hardBtn;
     @FXML
     private Button intermediateBtn;
+    @FXML
+    private Button backBtn;
 
     public void setStage(Stage stage) {
         this.stage = stage;
@@ -65,6 +65,20 @@ public class SinglemodeController implements Initializable {
             stage.setTitle(difficulty + " Mode");
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleBackBtn(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/tic/tac/toe/game/iti/client/Welcome.fxml"));
+            Parent root = loader.load();
+            WelcomeController controller = loader.getController();
+            controller.setStage(stage);
+            stage.setScene(new Scene(root));
+            stage.setTitle("Welcome Page");
+        } catch (IOException ex) {
+            Logger.getLogger(SinglemodeController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
